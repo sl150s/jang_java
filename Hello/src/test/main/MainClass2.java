ef072b0b7c1d5380219e1fb05a08438bf239f8b7
jangheejin <sl150s@naver.com>
2022-10-25 11:04:20 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/main/MainClass2.java
@@ -0,0 +1,27 @@
+//굵은 글씨 : 예약어 - 무언가의 식별자로 쓸수없음, 변수명등등.. 
+
+package test.main;
+//package 패키지명
+//이 클래스가 속해있는 패키지명을 밝혀주어야한다. 
+
+/*
+ * 여러줄 주석입니다.
+ * 하나
+ * 두울
+ * 세엣 
+ */
+
+//public class 클래스명
+//공개된(모든 패키지에서 사용할 수 있는) 클래스 , 클래스명은 MainClass2
+public class MainClass2 { //클래스의 시작
+	//한줄 주석입니다. 
+	
+	/*
+	 * 메소드명 : main
+	 * run 했을 때 실행이 시작되는 아주 특별한 메소드
+	 */
+	public static void main(String[] args) {
+		//main 치고 ctrl + space 클릭하면 자동완성됨
+	}
+	
+}//클래스의 끝
