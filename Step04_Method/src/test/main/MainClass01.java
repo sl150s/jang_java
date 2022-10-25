8dca68c874de9d5719464dafe519cb9d15f67648
jangheejin <sl150s@naver.com>
2022-10-25 15:38:53 +0900

    Step04에 MainClass01 예제 추가함

--- Step04_Method/src/test/main/MainClass01.java
+++ Step04_Method/src/test/main/MainClass01.java
@@ -1,6 +1,7 @@
 package test.main;
 
 import test.mypac.MyObject;
+import test.mypac.Car;
 
 public class MainClass01 {
 	public static void main(String[] args) {
@@ -13,9 +14,38 @@ public class MainClass01 {
 		
 		
 		MyObject obj = new MyObject();
+		
+		
 		obj.walk();
 		int num = obj.getNumber();
 		String get = obj.getGreeting();
+		Car car = obj.getCar();
+		
+		//setNum(9), setName(), useCar() 메소드도 호출해보세요.
+		
+		
+		
+		//메소드를 ㅎ출하면서 인자로 전달할 데이터를 즉석에서 만들어서 전달하기
+		obj.setNum(1);
+		obj.setName("안녕하세요");
+		obj.useCar(new Car());
+		
+		
+		
+		
+		//메소드 호출하면서 인자로 전달할 값이 지역변수에 이미 존재하는 경우라고 가정하자
+		int a = 999;
+		String b = "누구게";
+		Car c1 = new Car();
+		
+		//전달할 값이 들어 있는 변수명으로 전달할 수도 있다.
+		obj.setNum(a);
+		obj.setName(b);
+		obj.useCar(c1);
+		
+		
+		//doSomething() 메소드를 호출해보세요. 
+		obj.doSomething(a,b,c1);
 		
 	}
 }
--- Step04_Method/src/test/mypac/MyObject.java
+++ Step04_Method/src/test/mypac/MyObject.java
@@ -14,12 +14,48 @@ public class MyObject {
 	public void walk() {
 		System.out.println("걸음을 걸어요");
 	}
+	//void 어떤 타입도 리턴하지 않음. 
 	
 	public int getNumber() {
 		return 10; 
 	}
+	// int : int타입을 리턴해야한다 - 반드시 return 값이 있어야함 
 	
 	public String getGreeting() {
 		return "안녕하세요"; 
 	}
+	// String : String 타입을 리턴해야한다 - 반드시 return 값이 있어야함 
+	
+	//CAR TYPE을 리턴해주는 메소드
+	public Car getCar() {
+		return new Car();
+	}
+	
+	//int type을 메소드의 인자로 전달 받는 메소드
+	public void setNum(int num) {
+		System.out.println("num:"+num);
+	}
+	
+	//string type을 메소드의 인자로 전달 받는 메소드
+	public void setName(String name) {
+		System.out.println("name:"+name);
+	}
+	
+	//Car type을 메소드의 인자로 전달 받는 메솓
+	public void useCar(Car car) {
+		car.drive();
+	}
+	
+	// 3개의 type을 메소드의 인자로 전달 받는 메소드
+	public void doSomething(int num, String msg, Car c) {
+		System.out.println("무언가를 작업합니다.");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
