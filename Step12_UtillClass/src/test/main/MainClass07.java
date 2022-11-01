2cea74e1e5f674e86a643df8873182c9ce30bd4c
jangheejin <sl150s@naver.com>
2022-11-01 11:00:43 +0900

    Merge branch 'master' of https://github.com/sl150s/jang_java.git

---
+++ Step12_UtillClass/src/test/main/MainClass07.java
@@ -0,0 +1,17 @@
+package test.main;
+
+import java.util.HashSet;
+
+/*
+ * HashSet은 Set 인터페이스를 구현한 클래스이다. 
+ * 
+ * - 순서가 없다.
+ * - key값도 없다.
+ * - 중복을 허용하지 않는다,
+ * - 어떤 data를 묶음(집합)으로 관리하고자 할 때 사용한다.
+ */
+public class MainClass07 {
+	public static void main(String[] args) {
+		HashSet set1 = new HashSet<>();
+	}
+}
