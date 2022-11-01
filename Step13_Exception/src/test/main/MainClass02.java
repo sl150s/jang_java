4f9be1f67b522ae81917171835d053c00e4ba5dc
jangheejin <sl150s@naver.com>
2022-11-01 15:14:42 +0900

    Step13_Exception 프로젝트 추가

---
+++ Step13_Exception/src/test/main/MainClass02.java
@@ -0,0 +1,35 @@
+package test.main;
+
+import java.util.Scanner;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("나눌수 입력:");
+		String inputNum1=scan.nextLine();
+		System.out.println("나누어 지는 수 입력:");
+		String inputNum2=scan.nextLine();
+		
+		try {
+			int num1=Integer.parseInt(inputNum1);
+			int num2=Integer.parseInt(inputNum2);
+			
+			int result=num2/num1; //몫
+			int result2=num2%num1; //나머지
+			System.out.println(num2+" 를 "+num1+" 으로 나눈 몫 :" + result);
+			System.out.println(num2+" 를 "+num1+" 으로 나눈 나머지 :" + result2);
+		}catch(NumberFormatException nfe) {
+			System.out.println("숫자 형식으로 입력해 주세요");
+		}catch(ArithmeticException ae){
+			System.out.println("어떤 수를 0으로 나눌 수 없습니다.");
+		}catch(Exception e) {
+			System.out.println("예외가 발생했습니다.");
+		}finally { //finally : 위의 예외발생과 상관 없이 무조건 실행된다.
+			System.out.println("무언가 중요한 마무리 작업을 해요!");
+		}
+		
+		
+		System.out.println("main 메소드가 정상 종료됩니다.");
+	}
+}
