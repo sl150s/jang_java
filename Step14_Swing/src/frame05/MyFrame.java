338053253dfebc2b6fdce2d034754903fe214680
jangheejin <sl150s@naver.com>
2022-11-02 12:27:13 +0900

    Step_Swing 프로젝트에 예제 추가

--- Step14_Swing/src/frame05/MyFrame.java
+++ Step14_Swing/src/frame05/MyFrame.java
@@ -1,6 +1,7 @@
 package frame05;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -8,46 +9,72 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener{
+	//필드 (저장소) 역할 
+	JButton sendBtn;
+	JButton closeBtn;
+
+	
 	//생성자
 	public MyFrame(String title) {
 		super(title);
+		
+		//다형성 확인 : this 예약어를 이용해서 MyFrame 객체의 다형성 확인
+		MyFrame a = this;
+		JFrame b = this;
+		Component c = this;
+		Object d = this;
+		
 		setBounds(100, 100, 500, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new FlowLayout());
 		
-		JButton sendBtn = new JButton("전송");
+		//객체 생성해서 참조값을 필드에 저장 
+		sendBtn = new JButton("전송");
 		add(sendBtn);
 		
+		closeBtn = new JButton("삭제");
+		add(closeBtn);
 		
-		
-		//ActionListener 인터페이스 type의 참조값 얻어내서 
-		ActionListener listener = new ActionListener() {
-
-			@Override
-			//ActionListener 인터페이스가 가지고 있는 
-			//추상 메소드를 전부 오버라이드하는게 강제된다.
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("전송버튼이 눌렸습니다.");
-			}
-			
-		};
-		
-		
-		//전송버튼에 등록하기
-		sendBtn.addActionListener(listener);
-		
-	
-		
-		//setVisible(true);
+		sendBtn.addActionListener(this);
+		closeBtn.addActionListener(this);
 	}
 	
 	public static void main(String[] args) {
 		
-		MyFrame y = new MyFrame("나의 프레임5");
+		MyFrame y = new MyFrame("나의 프레임6");
 		y.setVisible(true);
 		
 	}
+	
+	/*
+	 * 전송버튼, 삭제버튼을 누르면 호출되는 메소드
+	 * 메소드 안에서 어떤 버튼을 눌렀는지 확인해서 분기해야 한다.
+	 * 메소드에 선언된 매개변수 ActionEvent e 에 눌러진 버튼에 대한 정보가 담겨 있따.
+	 * 해당 객체를 활용해서 분기를 하면 된다. 
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//눌러진 버튼의 참조값 얻어내기 
+		Object which = e.getSource();
+		
+		//생성자 안에서 생성된 변수는 지역변수 이기 때문에 다른 클래스에서 사용할 수 없음 
+		if(which == sendBtn) {
+			JOptionPane.showConfirmDialog(this,"전송");
+		}else if(which == closeBtn) {
+			JOptionPane.showConfirmDialog(this,"삭제");
+		}
+		 
+		//e의 메소드
+		// 1) .getSource() : 참조값이 전달된다. 
+		
+		
+		
+	}
+	
+	
+	
+	
 }
---
+++ Step14_Swing/src/frame06/MyFrame.java
@@ -0,0 +1,86 @@
+package frame06;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class MyFrame extends JFrame {
+	//생성자
+	public MyFrame(String title) {
+		super(title);
+		
+		//다형성 확인 : this 예약어를 이용해서 MyFrame 객체의 다형성 확인
+		MyFrame a = this;
+		JFrame b = this;
+		Component c = this;
+		Object d = this;
+		
+		setBounds(100, 100, 500, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		
+		JButton sendBtn = new JButton("전송");
+		add(sendBtn);
+		
+		//이너 클래스 사용
+		ActionListener listener = new ActionListener() {
+			//익명 클래스 사용 (Anonymous Class)
+			@Override
+			//ActionListener 인터페이스가 가지고 있는 
+			//추상 메소드를 전부 오버라이드하는게 강제된다.
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("전송버튼이 눌렸습니다.");
+				
+				JOptionPane.showMessageDialog(MyFrame.this, "전송합니다.");
+				
+				Component n = MyFrame.this;
+				ActionListener m = this; 
+				//그냥 this를 클랙할 경우 이너 클래스의 객체를 가리킨다. 
+				//이너 클래스를 포함하고 있는 아웃픗 클래스의 객체를 가리키고 싶으면 
+				// 아웃풋 클래스.this 로 표기한다. 예) Component n = MyFrame.this;
+			}
+			
+		};
+		
+		//전송버튼에 등록하기
+		sendBtn.addActionListener(listener);
+		
+		
+		
+		/* 예시 ))))))))))))))))))))))))))))))))))))))
+		 * 삭제 버튼도 프레임에 배치하고, 해당 버튼을 눌렀을 때 [삭제 버튼을 눌렀네] 라는
+		 * 메세지를 콘솔창에 출력되도록 프로그래밍 해보세요. 
+		 */
+		JButton closeBtn = new JButton("삭제");
+		add(closeBtn);
+		
+		//익명 클래스 사용 (Anonymous Class)
+		ActionListener closeClick = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("삭제 버튼을 눌렀네 ");
+				JOptionPane.showConfirmDialog(MyFrame.this, "삭제");
+			}};
+		closeBtn.addActionListener(closeClick);
+		//setVisible(true);
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		MyFrame y = new MyFrame("나의 프레임5");
+		y.setVisible(true);
+		
+	}
+}
